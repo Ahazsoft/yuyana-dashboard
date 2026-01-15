1b8c9363682e62f65031ac0bd7c3474476219ef5
EyobAbdella <eyobabdellasharo@gmail.com>
2026-01-15 16:53:40 +0300

    Implement sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import {
+  IconCalendarCheck,
+  IconCreditCard,
+  IconDashboard,
+  IconHelp,
+  IconMapPin,
+  IconMessageStar,
+  IconRoute,
+  IconSearch,
+  IconSettings
+} from "@tabler/icons-react";
+import * as React from "react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavSecondary } from "@/components/nav-secondary";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+
+const data = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/admin",
+      icon: IconDashboard,
+    },
+    {
+      title: "Destinations",
+      url: "/admin/destinations",
+      icon: IconMapPin,
+    },
+    {
+      title: "Tours",
+      url: "/admin/tours",
+      icon: IconRoute,
+    },
+    {
+      title: "Bookings",
+      url: "/admin/bookings",
+      icon: IconCalendarCheck,
+    },
+    {
+      title: "Payments",
+      url: "/admin/payments",
+      icon: IconCreditCard,
+    },
+    {
+      title: "Reviews",
+      url: "/admin/reviews",
+      icon: IconMessageStar,
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: IconHelp,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: IconSearch,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible='offcanvas' {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className='data-[slot=sidebar-menu-button]:!p-1.5'>
+              <a href='#' className='flex justify-start h-full'>
+                <div className='h-full'>
+                  <Image
+                    src='/yuyanlogo.png'
+                    width={60}
+                    height={100}
+                    className='object-cover'
+                    alt='Travel With Yuyan Logo'
+                  />
+                </div>
+                <span className='text-base font-semibold mt-2.5'>
+                  Yuyan Admin
+                </span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className='mt-auto' />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
